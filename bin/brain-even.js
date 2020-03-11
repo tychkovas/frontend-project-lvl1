4b3e0b004c088295967f0a7ca5f8d0628a4e8414
tychkovas <tychkovas@yandex.ru>
2020-03-12 00:39:36 +0400

    Step5, add brain even games

---
+++ bin/brain-even.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+import acquaint, { task, gameOver, congratulations } from '../src/index.js';
+
+// getting to know the player
+const name = acquaint();
+// start game
+console.log('Answer "yes" if the number is even, otherwise answer "no".');
+// Questions to the player
+let trueAnswer = 0;
+let number = -1;
+while (trueAnswer < 3) {
+  number = (Math.random() * 100 + 1).toFixed();
+  if (task(number, (number % 2 === 0) ? 'yes' : 'no')) {
+    trueAnswer += 1;
+  } else {
+    gameOver(name);
+    break;
+  }
+}
+if (trueAnswer === 3) congratulations(name);
--- src/index.js
+++ src/index.js
@@ -6,6 +6,21 @@ const acquaint = () => {
 
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
+  return name;
 };
 
+const task = (num, answerTrue = 'yes') => {
+  console.log(`Question: ${num}`);
+  const answer = readlineSync.question('Your answer:');
+  const result = answer === answerTrue;
+  if (result) console.log('Correct!');
+  else console.log(`"${answer} is wrogn answer ;(. Correct answer was "${answerTrue}"`);
+  return result;
+};
+
+const gameOver = (name) => console.log(`Let's try again, ${name}!`);
+
+const congratulations = (name) => console.log(`Congratulations, ${name}!`);
+
 export default acquaint;
+export { task, gameOver, congratulations };
