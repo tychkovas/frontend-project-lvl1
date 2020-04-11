0ca6931c77410241c5127ec89f78b805007cafbe
tychkovas <tychkovas@yandex.ru>
2020-04-11 11:50:41 +0400

    refactor (utils) getRandomNumber sets the range

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,12 @@
-const maxValueNumberDefault = 100;
-
-const getRandomNumber = (r = maxValueNumberDefault) => Math.trunc(Math.random() * r + 1);
+/**
+ * Function returns a random number from a given range
+ * @param {number} from Start of range
+ * @param {number} to End of range (inclusive)
+ * @returns {number} Rundom number
+ */
+const getRandomNumber = (from = 1, to = 100) => (
+  Math.trunc(Math.random() * (to - from + 1)) + from
+);
 
 export default getRandomNumber;
 export {
