db4e9eeb70ea4eed8fc1a45d678574d0f7d6c98d
tychkovas <tychkovas@yandex.ru>
2020-04-28 12:02:30 +0400

    refactor (engine) generator function is passed inside the runRound function

--- src/game-engine.js
+++ src/game-engine.js
@@ -3,16 +3,17 @@ import readlineSync from 'readline-sync';
 /**
  * @typedef  {string} TroundQuestion Question round game
  * @typedef  {string} TcorrectAnswer The correct answer to the question of the round of the game
- * @typedef  {[TroundQuestion,string]} TDataRound Round game Dataset
+ * @typedef  {[TroundQuestion, TcorrectAnswer]} TDataRound Round game Dataset
  */
 
 /**
  * Run a round of the game
- * @param {string} roundQuestion Question round game
- * @param {TcorrectAnswer} correctAnswer The correct answer to the question of the round of the game
+ * @param {() => TDataRound} getDataRound Logic one round game function return question
+ * and corresponding answer of the round ['round question', 'correct answer']
  * @returns {boolean} Returns `true` if user wins the round, else if he loses `false`
 */
-const runRound = (roundQuestion, correctAnswer) => {
+const runRound = (getDataRound) => {
+  const [roundQuestion, correctAnswer] = getDataRound();
   console.log(`Question: ${roundQuestion}`);
   const answerUser = readlineSync.question('Your answer:');
   const result = answerUser === correctAnswer;
@@ -30,8 +31,8 @@ const numberOfRoundsOfGame = 3;
  * Game engine
  * @description General logic of brain games
  * @param {string} gameTask Game task description
- * @param {() => TDataRound} getDataRound Logic one round game function
- * return question and corresponding answer of the round ['round question', 'correct answer']
+ * @param {() => TDataRound} getDataRound Logic one round game function return question
+ * and corresponding answer of the round ['round question', 'correct answer']
  */
 const runGame = (gameTask, getDataRound) => {
   // getting to know the player
@@ -43,11 +44,12 @@ const runGame = (gameTask, getDataRound) => {
   console.log(gameTask);
   // Questions to the player
   for (let trueAnswer = 0; trueAnswer < numberOfRoundsOfGame; trueAnswer += 1) {
-    if (!runRound(...getDataRound())) {
-      return console.log(`Let's try again, ${nameUser}!`);
+    if (!runRound(getDataRound)) {
+      console.log(`Let's try again, ${nameUser}!`);
+      return;
     }
   }
-  return console.log(`Congratulations, ${nameUser}!`);
+  console.log(`Congratulations, ${nameUser}!`);
 };
 
 export default runGame;
